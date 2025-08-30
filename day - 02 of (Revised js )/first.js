a742ac99821219bb532df8a18d8a9ca8a9042c75
Ankuj matre <154369381+ankuj07@users.noreply.github.com>
2025-08-30 20:09:56 +0530

    Add files via upload

---
+++ day - 02 of (Revised js )/first.js
@@ -0,0 +1,39 @@
+console.log("Hii lets can started");
+
+console.log("so started what is datatypes in the javascript");
+// many types of javascript like Number, string , bolean, bigint, undefined, null
+
+
+let num = 12;
+console.log(num);
+console.log(typeof num);
+
+let str = "Ankuj Matre";
+console.log(str);
+console.log("str");
+console.log(typeof str);
+
+const bool = true;
+console.log(bool);
+let boolean = false;
+console.log(typeof bool);
+console.log(typeof boolean);
+
+let a = 2334540943877276278676778; // cannot represent all value therefore we use bigint bslast me n lagana hota hai 
+console.log(a);
+let bigint = 2334540943877276278676778n;
+
+
+let c;
+console.log(c); // because hamane uskao define nhi kiya hai 
+
+
+let d = null;
+console.log(d); // ham khud se bol rahe hai ki value store mt karo ,
+
+
+// undefined ka matlab hai ki hamane define nhi kiye hai 
+// null ka matlab hai ki ham khud se hi use khali rakhana chahte hai 
+
+
+//  Todays task is completed ; bolo jindabad 
