ce4f5e0d7ef6973d4d06ebc7c40041e14af2f416
Ankuj matre <154369381+ankuj07@users.noreply.github.com>
2025-09-01 20:11:20 +0530

    Add files via upload

---
+++ day - 04 of ( Revised js )/script.js
@@ -0,0 +1,56 @@
+// So  lets can started 
+
+// non primitive datatypes : Array, object , function 
+
+let arr=[11, 22, 44, "Rohit", "virat"];
+console.log(arr);
+
+// Why we use objects becuase we collect similiar data ek hi jagah par
+
+let obj = {
+    user_name : 'Ankuj ',
+    password : 7828073096 ,
+    Village : 'Payali ',
+    Location : 'Balaghat' 
+}
+console.log(obj);
+console.log(typeof obj);
+
+// talk about function 
+
+let fun = function (){
+    console.log("hii i am ankuj");
+ 
+}
+
+fun();
+
+// convertion one time to another type 
+
+let bro = "Ankuj";
+let num = Number(bro);
+
+console.log(typeof bro);
+console.log(typeof num);
+
+let a = true;
+let b = Number(a);
+
+console.log(typeof a);
+console.log(typeof b);
+
+let namee = 100000;
+let change = String(namee);
+
+console.log(typeof namee);
+console.log(typeof change);
+
+let bool = true;
+let changing = String(bool);
+
+console.log(typeof bool);
+console.log(typeof changing);
+
+
+console.log(10%2);
+console.log(4+2*3-4/3); 4 + 6 - 1.333
