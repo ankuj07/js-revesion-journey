f13643a8bffbd8009946b6bc08749db9dd6b6f22
Ankuj matre <154369381+ankuj07@users.noreply.github.com>
2025-09-04 20:42:05 +0530

    Add files via upload

---
+++ day - 07 of ( Revised js )/script.js
@@ -0,0 +1,42 @@
+// console.log("Lets can started lecture 07");
+
+// let num1 = 123456;
+// let num2 = new Number(123456);
+// let num3 = new Number(123456);
+
+// // ham sb ko compare karake check kar sakate hai 
+// console.log(num1);   // ye ek number hai na ki object
+// console.log(num2);   // ye object hai jisaka adress bs stack me hai 
+// console.log(num3);   // ye bhi object hi hai 
+
+// // comparing 
+// console.log(num1 == num2); // output true hoga because num1 no. hai jabaki num2 objects hai , object change hoga no. me to dono no. ho jaaenge
+// console.log(num2 == num3); // false hoga because dono object ka address alg alg hai 
+
+// console.log(num2); 
+// console.log(typeof num2);
+// console.log(typeof num1);
+// console.log(typeof num3);
+
+// // we can check another chije 
+
+// let num = 231.68;
+// console.log(num.toFixed(3));  // points ke baad kitane value chahiye ye tay hota hai
+// console.log(num.toExponential(4));
+// console.log(num.toLocaleString(6));
+// console.log(num.toPrecision(4));
+// console.log(typeof num.toString());
+
+
+// Starting maths in js 
+
+// console.log(Math.E);
+// console.log(Math.LN10);
+// console.log(Math.LOG10E);
+// console.log(Math.PI);
+
+// how to generate random no.
+
+// console.log(Math.random());  // range = 0-1 ke bich
+// console.log(Math.random()*10); // output = 0-9
+// console.log(Math.random()*10+1)
