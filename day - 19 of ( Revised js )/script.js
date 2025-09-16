6a10e6cb74294132cac7e393dbf276d2ba1d9019
Ankuj matre <154369381+ankuj07@users.noreply.github.com>
2025-09-16 18:43:39 +0530

    Add files via upload

---
+++ day - 19 of ( Revised js )/script.js
@@ -0,0 +1,5 @@
+// Lets started dom (Document object model)
+
+// with the help of dom we can change in our html // because dom change html code in object and we can access the object in js easily
+
+console.log("I am Ankuj");
